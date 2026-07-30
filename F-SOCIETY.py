a34cdd710a037ee8c4111ec86b514391ef4db1f6
Nexus <almendaressantos07@gmail.com>
2026-07-30 00:48:45 -0600

    F-SOCIETY.py

--- F-SOCIETY.py
+++ F-SOCIETY.py
@@ -40,7 +40,7 @@ def escanear_red():
         for host in escaner.all_hosts():
             print(f"\n{Style.BRIGHT}{Fore.CYAN}[+] IP: {host}{Style.RESET_ALL}") 
             print(f"{Style.BRIGHT}{Fore.CYAN}[+] Estado: {escaner[host].state()}{Style.RESET_ALL}") 
-            if escaner[host].hostname(): 
+        if escaner[host].hostname():
             print(f"{Style.BRIGHT}{Fore.CYAN}[+] Nombre: {escaner[host].hostname()}{Style.RESET_ALL}")
             print("-" * 30)
     except:
